75b628bc5526344dfcdfe9113bd2d109778c6680
Anna Farafonova <annie.hop@yandex.ru>
2023-09-17 00:42:27 +0300

    test3

--- errors/ConflictingRequest.js
+++ errors/ConflictingRequest.js
@@ -1,9 +1,18 @@
-const { HTTP_STATUS_CONFLICT } = require('http2').constants;
+// const { HTTP_STATUS_CONFLICT } = require('http2').constants;
 
-module.exports = class ConflictingRequest extends Error {
-  constructor(massage) {
-    super(massage);
-    this.statusCode = HTTP_STATUS_CONFLICT;
+// module.exports = class ConflictingRequest extends Error {
+//   constructor(massage) {
+//     super(massage);
+//     this.statusCode = HTTP_STATUS_CONFLICT;
+//   }
+// };
+
+class ConflictingRequest extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = 409;
   }
-};
+}
+
+module.exports = ConflictingRequest;
 // 409
