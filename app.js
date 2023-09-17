09a556d852bb4cd98ec92755d64bce2c3a3d93b2
Anna Farafonova <annie.hop@yandex.ru>
2023-09-17 12:56:31 +0300

    test3

--- app.js
+++ app.js
@@ -75,14 +75,14 @@ app.use(errorLogger);
 app.use(errors());
 
 app.use((err, req, res, next) => {
-  // const { statusCode = 500, message } = err;
-  // res
-  //   .status(statusCode)
-  //   .send({
-  //     message: statusCode === 500
-  //       ? 'На сервере произошла ошибка'
-  //       : message,
-  //   });
+  const { statusCode = 500, message } = err;
+  res
+    .status(statusCode)
+    .send({
+      message: statusCode === 500
+        ? 'На сервере произошла ошибка'
+        : message,
+    });
   next();
 });
 
